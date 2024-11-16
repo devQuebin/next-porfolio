3f0a79a5626049d5e1edd29524f9778c73419026
Quebin <kevin.matsuda@hks.ar>
2024-11-16 16:48:08 -0300

    routing fix

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -11,7 +11,6 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
-  Text,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { HiTranslate } from "react-icons/hi";
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(es|en)/:path*"],
+  matcher: ["/", "/(es|en)/:path*", "/((?!api|_next|_vercel|.*\\..*).*)"],
 };
